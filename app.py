10cba4371b89c82adb8081cd6bd749d8f6768660
VunKanha <vykanha111@gmail.com>
2024-07-20 10:27:41 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,41 @@
+# print("Hello Student" + " From BBU"  + str(5))#convert num to str use str
+# print("Hi")
+#
+# print("Single 'quote' inside double quote")
+# print('Double "quote" single quote')
+# print("singel \'quote\' and \"double\" quote \n inside quote")
+
+
+
+#name=input("What is your name?")
+#print("Welcome " + name)
+
+# qty1=int(input("qty1="))
+# qty2=int(input("qty2="))
+# price=float(input("price="))
+# total=qty1+qty2
+# amount=total*price
+# print("total qty" + str(total))
+# print("amount" + str(amount))
+# #print("amount" + amount)
+
+# There are two variable , a and b from input
+# a = input()
+# b = input()
+
+# Dont change the code above
+# Write your code below this line
+
+# c=a
+# a=b
+# b=c
+#
+# print("a:" + a)
+# print("b:" + b)
+
+print("Welcome to the band name generator")
+city=input("Which city did you grow up in?\n")
+pet_name=input("What is the name of a pet?\n")
+print("Your band name could be " + city +" "+ pet_name)
+
+
